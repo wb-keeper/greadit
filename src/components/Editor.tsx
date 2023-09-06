a60165b73c3ee69b16de42cb5438c3ad6f52285e
wb-keeper <wb-keeper@mail.ru>
2023-09-06 19:43:27 +0300

    finishing working on editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React, { FC, useCallback, useRef } from "react";
+import React, { FC, useCallback, useEffect, useRef, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import { useForm } from "react-hook-form";
 import { PostCreationRequest, PostValidator } from "@/lib/validators/post";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type EditorJS from "@editorjs/editorjs";
+import { uploadFiles } from "@/lib/uploadthing";
+import { toast } from "@/hooks/use-toast";
 interface EditorProps {
   subgreaditId: string;
 }
@@ -24,6 +26,8 @@ const Editor: FC<EditorProps> = ({ subgreaditId }) => {
   });
 
   const ref = useRef<EditorJS>();
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
 
   const initializeEditor = useCallback(async () => {
     const EditorJs = (await import("@editorjs/editorjs")).default;
@@ -57,22 +61,91 @@ const Editor: FC<EditorProps> = ({ subgreaditId }) => {
             class: ImageTool,
             config: {
               uploader: {
-                async uploadByFile(file: File) {},
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], "imageUploader");
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  };
+                },
               },
             },
           },
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
         },
       });
     }
   }, []);
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      setIsMounted(true);
+    }
+  }, []);
+  useEffect(() => {
+    if (Object.keys(errors).length) {
+      for (const [_key, value] of Object.entries(errors)) {
+        toast({
+          title: "Something went wrong",
+          description: (value as { message: string }).message,
+          variant: "destructive",
+        });
+      }
+    }
+  }, [errors]);
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+      setTimeout(() => {
+        _titleRef.current?.focus();
+      }, 0);
+    };
+    if (isMounted) {
+      init();
+      return () => {
+        ref.current?.destroy();
+        ref.current = undefined;
+      };
+    }
+  }, [isMounted, initializeEditor]);
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save();
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subgreaditId,
+    };
+  }
+  if (!isMounted) {
+    return null;
+  }
+  const { ref: titleRef, ...rest } = register("title");
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
-      <form id="subgreadit-post-form" className="w-fit" onSubmit={() => {}}>
+      <form
+        id="subgreadit-post-form"
+        className="w-fit"
+        onSubmit={() => {
+          handleSubmit((e) => {});
+        }}
+      >
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+            ref={(e) => {
+              titleRef(e);
+              // @ts-ignore
+              _titleRef.current = e;
+            }}
+            {...rest}
             placeholder="Title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
+          <div id="editor" className="min-h-[500px]" />
         </div>
       </form>
     </div>
