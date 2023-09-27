405e466db779ff15d71363d289b576eff31dc031
wb-keeper <wb-keeper@mail.ru>
2023-09-27 19:44:51 +0300

    Finished the upvotes

--- src/app/api/subgreadit/post/vote/route.ts
+++ src/app/api/subgreadit/post/vote/route.ts
@@ -2,11 +2,13 @@ import { PostVoteValidator } from "@/lib/validators/votes";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { CachedPost } from "@/types/redis";
+import { redis } from "@/lib/redis";
+import { z } from "zod";
 
 const CACHE_AFTER_UPVOTES = 1;
 export async function PATCH(req: Request) {
   try {
-    const body = req.json();
+    const body = await req.json();
     const { postId, voteType } = PostVoteValidator.parse(body);
     const session = await getAuthSession();
     if (!session?.user) {
@@ -67,7 +69,44 @@ export async function PATCH(req: Request) {
           currentVote: voteType,
           createdAt: post.createdAt,
         };
+        await redis.hset(`post:${postId}`, cachePayload);
       }
+      return new Response("OK");
     }
-  } catch (e) {}
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    const votesAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === "UP") return acc + 1;
+      if (vote.type === "DOWN") return acc - 1;
+      return acc;
+    }, 0);
+    if (votesAmt >= CACHE_AFTER_UPVOTES) {
+      const cachePayload: CachedPost = {
+        authorUserName: post.author.username ?? "",
+        content: JSON.stringify(post.content),
+        id: post.id,
+        title: post.title,
+        currentVote: voteType,
+        createdAt: post.createdAt,
+      };
+      await redis.hset(`post:${postId}`, cachePayload);
+    }
+    return new Response("OK");
+  } catch (e) {
+    if (e instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+    return new Response(
+      "Could not register your vote, please try again later",
+      {
+        status: 500,
+      }
+    );
+  }
 }
